d40fa60a8709ee978b51998d821f446f26d9cc2f
kong <kong@kongui-MacBookPro.local>
2020-10-30 22:50:22 +0900

    second homework

---
+++ Week2_homework/Week2_homework/ViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ViewController.swift
+//  Week2_homework
+//
+//  Created by kong on 2020/10/30.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var profilebtn: UIButton!
+    @IBOutlet weak var ScrollView: UIScrollView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        profilebtn.layer.cornerRadius = 10
+        // Do any additional setup after loading the view.
+//
+//    (void)ScrollView:(UIScrollView *)sender {
+//        if(mScrollView.contentOffset.y > (mScrollView.contentSize.height -
+//            mScrollView.frame.size.height)/2) {
+//
+//        [topBtn setHidden:NO];
+//
+//        } else {[topBtn setHidden:YES];}
+//}
+//
+//-(IBAction) scrollToTop:(id)sender {
+//    [mScrollView setContentOffset:CGPointZero animated:NO];
+//
+//            }
+}
+}
+
